18461bf9548afddb4800bde7db425300b36c039d
kimi2129 <thuhoai2129@gmail.com>
2021-12-13 10:19:37 +0700

    update

--- apps/chart_types/threeD.py
+++ apps/chart_types/threeD.py
@@ -9,7 +9,8 @@ def Surface_Plots():
 
     fig = go.Figure(data=[go.Surface(z=z_data.values)])
 
-    fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+    fig.update_layout(title='Mt Bruno Elevation',
+                      #autosize=False,
                     #width=500, height=500,
                     #margin=dict(l=65, r=50, b=65, t=90)
                       )
